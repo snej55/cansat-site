7578ba69d0b8a8b751bc0630eada4147109edef4
Jan Lukasiak <j4mikl@gmail.com>
2025-05-09 11:05:53 +0100

    toggle background

--- cookies.js
+++ cookies.js
@@ -2,10 +2,28 @@ if (document.cookie == "") {
 document.cookie = "background: 0;"
 }
 
-document.cookie = "background: 0;"
+function update() {
+    if (document.cookie.trim().startsWith("background: 1")) {
+        document.body.style.backgroundColor = "#000"
+    }
+    else {
+        document.body.style.backgroundColor = "#fff"
 
-if (document.cookie.background == 1) {
-    document.body.style.backgroundColor = "#000"
+    }
 }
+
+function toggleBackground() {
+    if (document.cookie.trim().startsWith("background: 0")) {
+        document.cookie = "background: 1;"
+    }
+    else {
+        document.cookie = "background: 0;"
+    }
+    update()
+console.log(document.cookie)
+
+}
+
+update()
 
 console.log(document.cookie)
