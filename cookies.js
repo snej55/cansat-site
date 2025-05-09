98b7b27e67f57690c93109d1e91212411095252a
Jan Lukasiak <j4mikl@gmail.com>
2025-05-09 08:46:14 +0100

    cookies

--- cookies.js
+++ cookies.js
@@ -1,5 +1,11 @@
 if (document.cookie == "") {
-document.cookie = "background: 1;"
+document.cookie = "background: 0;"
+}
+
+document.cookie = "background: 0;"
+
+if (document.cookie.background == 1) {
+    document.body.style.backgroundColor = "#000"
 }
 
 console.log(document.cookie)
