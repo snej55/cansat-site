59c7c221fa71484d7936ee15beb4189201ef43c1
Jan Lukasiak <j4mikl@gmail.com>
2025-05-14 09:45:56 +0100

    documentation

--- website/ducumentation.js
+++ website/ducumentation.js
@@ -1,7 +1,7 @@
 function docs() {
     item.innerHTML = `<h1>Documentation</h1>
     <h2>First Prototype</h2><br>
-    We started to make the prototype right away to see if we could
+    We started to make the prototype right away to see what we would need
 `
     document.getElementById("credits").innerHTML = "Credits";
     document.getElementById("credits").setAttribute( "onClick", "javascript: credits();" );
