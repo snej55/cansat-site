5c46b55292971feb23f0d6060c31fc597b082f0c
Dylan Greenwood <dyloaf@icloud.com>
2025-05-14 10:16:26 +0100

    credits text

--- website/credits.js
+++ website/credits.js
@@ -2,6 +2,30 @@ function credits() {
 
     item.innerHTML = `<h1>Credits</h1>
     <h2>cansat</h2>
+    landing gear design:
+    Dylan greenwood
+    Nathan yin
+
+    logo design:
+    nick zyuzin
+
+    web design:
+    jan Lukasiak
+    jens kromdijk
+    dylan greenwood
+
+    parachute design:
+    sabastian thorton
+
+    prototyping:
+    dylan greenwood
+    nathan yin
+
+    code for satelite:
+    nathan yin
+    jens kromdijk
+
+
 `
     menuOpen = false;
     document.getElementById("credits").innerHTML = "Home";
