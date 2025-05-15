0c13f7bf02702c2b8322c9127e26959e6ec31a86
snej55 <jenskromdijk@gmail.com>
2025-05-15 12:17:46 +0100

    Add buzzer interface

--- src/buzzer.py
+++ src/buzzer.py
@@ -12,9 +12,8 @@ def landed():
         buzzer.duty_u16(0)
         time.sleep(0.5)
         led.on()
-        buzzer.duty_u16(500) # Change the duty cycle for volume control 0 to 32767, anythink higher will damage the buzzer
+        buzzer.duty_u16(3000) # Change the duty cycle for volume control 0 to 32767, anythink higher will damage the buzzer
         time.sleep(0.5)
 
 # time.sleep(5)
 landed()
-
