8e731b70d1e0c7ed3d7b3faf38d4f63a6322023f
Dylan Greenwood <dyloaf@icloud.com>
2025-05-15 14:18:42 +0100

    document

--- website/ducumentation.js
+++ website/ducumentation.js
@@ -1,7 +1,9 @@
 function docs() {
     item.innerHTML = `<h1>Documentation</h1>
     <h2>First Prototype</h2><br>
-    We started to make the prototype right away to see what we would need
+    We started to make the prototype right away to see what we would need for the design and construction of the satelite. <br>
+    we done this to leve as much of our budget for the final build and not all for the protottype to give us the best <br>
+    satelite we can get. 
 `
     document.getElementById("credits").innerHTML = "Credits";
     document.getElementById("credits").setAttribute( "onClick", "javascript: credits();" );
