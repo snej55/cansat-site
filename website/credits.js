99f629ebe9050abefeb319d599b9d1405f364b0d
Jan Lukasiak <j4mikl@gmail.com>
2025-05-15 10:17:42 +0100

    fixed teh credits page

--- website/credits.js
+++ website/credits.js
@@ -1,31 +1,30 @@
 function credits() {
 
     item.innerHTML = `<h1>Credits</h1>
-    <h2>cansat</h2>
-    <br>
-    <b>landing gear design:</b> <br>
+
+    <h2>landing gear design:</h2>
     Dylan greenwood <br>
-    Nathan yin <br>
- <br>
-   <b> logo design and team name:</b> <br>
+    Nathan yin <br><br>
+
+   <h2> logo design and team name:</h2>
     Nick zyuzin <br>
-    Sebastian thornton <br>
- <br>
-   <b> web design:</b> <br>
+    Sebastian thornton <br><br>
+
+   <h2> web design:</h2>
     Jan Lukasiak <br>
     Jens kromdijk <br>
     Dylan greenwood <br>
-    Sebastian thornton
- <br>
-   <b> parachute design:</b> <br>
+    Sebastian thornton <br><br>
+
+   <h2> parachute design:</h2>
     Sebastian thornton <br>
+    Dylan greenwood <br><br>
+
+   <h2> prototyping:</h2>
     Dylan greenwood <br>
- <br>
-   <b> prototyping:</b> <br>
-    Dylan greenwood <br>
-    Nathan yin <br>
- <br>
-   <b> code for satelite:</b> <br>
+    Nathan yin <br><br>
+
+   <h2> code for satelite:</h2>
     Nathan yin <br>
     Jens kromdijk <br>
 
