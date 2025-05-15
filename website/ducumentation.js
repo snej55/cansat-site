3cb98fc557d903cec82a40155ccffe54d9561842
J4mik <j4mikl@gmail.com>
2025-05-15 17:40:06 +0100

    ducumentation

--- website/ducumentation.js
+++ website/ducumentation.js
@@ -1,9 +1,17 @@
 function docs() {
-    item.innerHTML = `<h1>Documentation</h1>
-    <h2>First Prototype</h2><br>
+    item.innerHTML = `<h1>Documentation </h1> <br>
+    <h2>First Prototype </h2> 
     We started to make the prototype right away to see what we would need for the design and construction of the satelite. <br>
     we done this to leve as much of our budget for the final build and not all for the protottype to give us the best <br>
-    satelite we can get out of the budget we have.
+    satelite we can get out of the budget we have. <br><br>
+    <h3>What we made </h3>
+    <ul>
+        <li>outer case prototype </li>
+        <li>schematic </li>
+        <li>prototype parachute </li>
+    </ul> <br>
+    hello
+
 `
     if (!mobileView) {
         document.getElementById("credits").innerHTML = "Credits";
