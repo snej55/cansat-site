a18c75fe62dd7063c530e3b7d66009d29ffc9f24
Dylan Greenwood <dyloaf@icloud.com>
2025-05-15 14:20:14 +0100

    document

--- website/ducumentation.js
+++ website/ducumentation.js
@@ -3,7 +3,7 @@ function docs() {
     <h2>First Prototype</h2><br>
     We started to make the prototype right away to see what we would need for the design and construction of the satelite. <br>
     we done this to leve as much of our budget for the final build and not all for the protottype to give us the best <br>
-    satelite we can get out of the bbudet we have.
+    satelite we can get out of the budget we have.
 `
     document.getElementById("credits").innerHTML = "Credits";
     document.getElementById("credits").setAttribute( "onClick", "javascript: credits();" );
