daf8821bde61caa25f4cd08d9826388a26e46de7
J4mik <j4mikl@gmail.com>
2025-05-16 16:22:33 +0100

    update credits

--- website/credits.js
+++ website/credits.js
@@ -2,38 +2,28 @@ function credits() {
 
     item.innerHTML = `<h1>Credits</h1><br>
 
-
-   <h3>Logo Design:</h3>
+    <h3>Logo Design:</h3>
     Nick Zyuzin <br>
-    Sebastian Thornton 
+    Sebastian Thornton <br><br>
 
-   <h3>Web Design:</h3>
+    <h3>Website:</h3>
     Jan Lukasiak <br>
     Jens kromdijk <br>
     Dylan Greenwood <br>
     Sebastian Thornton <br><br>
 
-
     <h3>Prototyping:</h3>
-    <br>Nathan yin <br>
-    Dylan greenwood <br>
+    Nathan yin <br>
+    Dylan greenwood <br><br>
     
-
-   <h3> code for satelite:</h3>
+    <h3> code for satelite:</h3>
     Nathan yin <br>
     Jens kromdijk <br>
     Jan Lukasiak <br><br>
 
-    <h3>testing:</h3>
-    Dylan greenwood <br>
-    Sebastian thornton<br><br>
-
     <h3>3d modeling and printing:</h3>
     Nathan yin <br>
     Dylan greenwood <br>
-    
-
-
 `
     if (!mobileView) {
         document.getElementById("credits").innerHTML = "Home";
