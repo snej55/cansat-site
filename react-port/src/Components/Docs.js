3d84f9d80959c11acc64cb8fe523ea85069014a5
snej55 <jenskromdijk@gmail.com>
2025-05-18 14:05:47 +0100

    Add styling to docs

--- react-port/src/Components/Docs.js
+++ react-port/src/Components/Docs.js
@@ -1,17 +1,23 @@
+import './Docs.css';
+
 export function Docs() {
     return (
         <div className="page-content">
-            <h2>First Prototype</h2>
-            Before starting work on the actual satellite, we first worked on developing an initial prototype
-            for the satellite.
-            <br></br>
-            <h3>What we made: </h3>
-            <ul>
-                <li>Outer case prototype</li>
-                <li>Schematic</li>
-                <li>Prototype parachute</li>
-            </ul>
-            <img src="screenshots/Screenshot 2025-05-15 at 19.37.39 - transparent.png" alt="cad model"></img>
+            <div className="docs-text-image-block">
+                <img className="docs-screenshot" src="screenshots/Screenshot 2025-05-15 at 19.37.39 - transparent.png" alt="cad model"></img>
+                <div className="docs-text-block">
+                    <h2>First Prototype</h2>
+                    Before starting work on the actual satellite, we first worked on developing an initial prototype
+                    for the satellite.
+                    <br></br>
+                    <h3 className="docs-subheading">What we made: </h3>
+                    <ul className="docs-bullets">
+                        <li><div>Outer case prototype</div></li>
+                        <li><div>Schematic</div></li>
+                        <li><div>Prototype parachute</div></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
