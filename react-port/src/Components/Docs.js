76e909f55b819a3ec13e83de4a83cfe3f640abc6
snej55 <jenskromdijk@gmail.com>
2025-05-18 13:40:41 +0100

    Add screenshot to docs page

--- react-port/src/Components/Docs.js
+++ react-port/src/Components/Docs.js
@@ -4,6 +4,14 @@ export function Docs() {
             <h2>First Prototype</h2>
             Before starting work on the actual satellite, we first worked on developing an initial prototype
             for the satellite.
+            <br></br>
+            <h3>What we made: </h3>
+            <ul>
+                <li>Outer case prototype</li>
+                <li>Schematic</li>
+                <li>Prototype parachute</li>
+            </ul>
+            <img src="screenshots/Screenshot 2025-05-15 at 19.37.39 - transparent.png" alt="cad model"></img>
         </div>
     )
 }
