3fcffe138af56e1f75fc732b514d755ab70d5be9
snej55 <jenskromdijk@gmail.com>
2025-05-18 13:38:15 +0100

    Add content to docs and screenshots

--- react-port/src/Components/Docs.js
+++ react-port/src/Components/Docs.js
@@ -1,7 +1,9 @@
 export function Docs() {
     return (
         <div className="page-content">
-            Welcome to the documentation page!
+            <h2>First Prototype</h2>
+            Before starting work on the actual satellite, we first worked on developing an initial prototype
+            for the satellite.
         </div>
     )
 }
