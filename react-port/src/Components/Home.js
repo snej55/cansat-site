80e126edf55003a5389338b75f9b21875e2cce83
snej55 <jenskromdijk@gmail.com>
2025-05-19 20:07:21 +0100

    Theme switch demo

--- react-port/src/Components/Home.js
+++ react-port/src/Components/Home.js
@@ -1,7 +1,14 @@
+import './Home.css';
+import { useState } from 'react';
+
 export function Home() {
+    const [darkMode, setDarkMode] = useState(false);
     return (
         <div className="page-content">
-            Welcome to the home page!
+            <div className={darkMode ? "theme-wrapper-dark" : "theme-wrapper-light"}>
+                Welcome to the home page!
+                <button onClick={() => {setDarkMode(!darkMode)}}>Change theme</button>
+            </div>
         </div>
     )
 }
