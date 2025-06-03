39e0e0716735518e9bce3fcc3f3df0a8015acb31
Jan Lukasiak <j4mikl@gmail.com>
2025-06-03 16:48:12 +0100

    made documentation match rest of website

--- react-port/src/Components/Docs.js
+++ react-port/src/Components/Docs.js
@@ -2,19 +2,23 @@ import './Docs.css';
 
 export function Docs() {
     return (
-        <div className="page-content">
-            <div className="docs-text-image-block">
-                <img className="docs-screenshot" src="screenshots/Screenshot 2025-05-15 at 19.37.39 - transparent.png" alt="cad model"></img>
-                <div className="docs-text-block">
+        <div>
+            <div id="allign">
+                <div id="left">
+                    <img className="docs-screenshot" src="screenshots/Screenshot 2025-05-15 at 19.37.39 - transparent.png" alt="cad model"></img>
+                </div>
+                <div id="right">
                     <h2>First Prototype</h2>
-                    Before starting work on the actual satellite, we first worked on developing an initial prototype
-                    for the satellite.
-                    <br></br>
+                    <p>
+                        Before starting work on the actual satellite, we first worked on developing an initial prototype
+                        for the satellite.
+                    </p>
+                    <br />
                     <h3 className="docs-subheading">What we made: </h3>
                     <ul className="docs-bullets">
-                        <li><div>Outer case prototype</div></li>
-                        <li><div>Schematic</div></li>
-                        <li><div>Prototype parachute</div></li>
+                        <li>outer case prototype</li>
+                        <li>Schematic</li>
+                        <li>Prototype parachute</li>
                     </ul>
                 </div>
             </div>
--- react-port/src/Components/Mission.js
+++ react-port/src/Components/Mission.js
@@ -1,13 +1,13 @@
 export function Mission() {
     return (
         <div>
-            <h1>Our Mission</h1>{/*<br>*/}
-            {/* </br> */}
+            <h1>Our Mission</h1><br />
+            
             CastAway is the team behind the CastAway satelite, our skybreaking attempt to prove that 
             the sky is indeed the limit. Consisting of six multitalented junior individuals, we have 
             designed a satelite to conduct various science experiments, while still meeting the design 
-            criteria set by ESERO. The satelite is set to perform the following science experiments:{/*<br>*/}
-            {/* <br> */}
+            criteria set by ESERO. The satelite is set to perform the following science experiments: <br />
+
             <ul>
                 <li>Measure the air pressure in various points of the atmosphere, and attempt to infer 
                 altitude from it.</li>
@@ -21,19 +21,19 @@ export function Mission() {
                 <li>Measure the GPS position of the satelite relative to Earth in three dimensions.</li>
                 <li>Take pretty pictures!</li>
             </ul>
-            {/* </br> */}
+            <br />
             We need to have a near-constant steam of data sent to us over radio, and be able to recover 
-            the satelite in a state where we can recover more data, and use it again.{/*<br>*/}
-            {/* </br> */}
+            the satelite in a state where we can recover more data, and use it again. <br />
+            <br />
             Another one of our objectives is to raise public awareness about our project, which we 
-            are trying to achieve by:{/*<br></br>*/}
+            are trying to achieve by: <br /><br />
             <ul>
                 <li>Making this website.</li>
                 <li>Giving presentations to the local education community.</li>
                 <li>Making flyers.</li>
                 <li>Gaining local media coverage.</li>
             </ul>
-            {/* <br> */}
+            <br />
             The CastAway satelite will be launched by a small rocket to an altitude of 1000 ft (300 m), 
             from which point it will start collecting data. It will deploy a parachute at its highest 
             altitude, in order to slow its rate of descent to between 10 and 15 m/s (22 and 34 mph). 
