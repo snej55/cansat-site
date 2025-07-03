a200eedd5988412c5ae794085baf1599fd6e5b16
snej55 <jenskromdijk@gmail.com>
2025-07-03 12:27:10 +0100

    Update

--- src/cansat/rfm69.py
+++ src/cansat/rfm69.py
@@ -13,6 +13,6 @@ def create_rfm69() -> adafruit_rfm69.RFM69:
     # Initialize the radio
     rfm = adafruit_rfm69.RFM69(spi, cs, reset, 434.0, baudrate=2000000)
     encryption_key = "VB6CYeaOtduNZcgu"
-    rfm.encryption_key = encryption_key
+    #rfm.encryption_key = encryption_key
     rfm.tx_power = 13
     return rfm
