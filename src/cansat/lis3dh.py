23895d31a941cef3f60cfda836ee17efa0ad545e
zibuyin <natdrone101@gmail.com>
2025-07-03 11:46:02 +0100

    updaed lis3dh

--- src/cansat/lis3dh.py
+++ src/cansat/lis3dh.py
@@ -1,16 +1,27 @@
 """
 ***** LIS3DH 3-axis accelerometer *****
+https://docs.circuitpython.org/projects/lis3dh/en/latest/api.html
 """
 
 import board
 import busio
 import adafruit_lis3dh
 
-def create_lis3dh() -> adafruit_lis3dh.LIS3DH_I2C:
+def create_lis3dh(sda,scl):
+
     # create i2c bus
-    i2c = busio.I2C(board.GP1, board.GP0)
-    # create acceleromter instance
+    # Create I2C bus
+    i2c = busio.I2C(sda, scl)
+
+    # Create LIS3DH accelerometer instance
     lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c)
     # optionally set range (default is RANGE_2_G)
     #lis3dh.range = adafruit_lis3dh.RANGE_2_G
     return lis3dh
+def reading_lis3dh(lis3dh):
+    
+    acc_x, acc_y, acc_z = lis3dh.acceleration
+    return [acc_x,acc_y,acc_z]
+
+lis3dh = create_lis3dh(board.GP1, board.GP0)
+print(reading_lis3dh(lis3dh))
