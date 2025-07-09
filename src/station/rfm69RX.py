d01d52867d149f17926e8f00b2980319930784e1
snej55 <jenskromdijk@gmail.com>
2025-07-09 09:11:10 +0100

    merge

--- src/station/rfm69RX.py
+++ src/station/rfm69RX.py
@@ -15,7 +15,7 @@ reset = digitalio.DigitalInOut(board.GP13)
 # Initialize the radio
 rfm = adafruit_rfm69.RFM69(spi, cs, reset, 434.0, baudrate=2000000)
 encryption_key = "VB6CYeaOtduNZcgu"
-#rfm.encryption_key = encryption_key
+rfm.encryption_key = encryption_key
 
 print("CanSat Ground Station Init")
 
